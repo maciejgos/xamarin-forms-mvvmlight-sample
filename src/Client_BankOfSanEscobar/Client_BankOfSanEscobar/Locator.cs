ad415314c78286dae42bf3834019ff70141041d9
Maciej Gos <maciej@maciejgos.com>
2017-01-16 21:20:25 +0100

    Create Locator

---
+++ src/Client_BankOfSanEscobar/Client_BankOfSanEscobar/Locator.cs
@@ -0,0 +1,13 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace Client_BankOfSanEscobar
+{
+    public class Locator
+    {
+        public Locator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+        }
+    }
+}
