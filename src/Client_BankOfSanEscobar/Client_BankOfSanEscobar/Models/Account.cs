b33aca879e894c39aaff364e5fddc29362c5f422
Maciej Gos <maciej@maciejgos.com>
2017-01-16 21:11:05 +0100

    Add Account model

---
+++ src/Client_BankOfSanEscobar/Client_BankOfSanEscobar/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace Client_BankOfSanEscobar.Models
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Balance { get; set; }
+        public string Number { get; set; }
+    }
+}
