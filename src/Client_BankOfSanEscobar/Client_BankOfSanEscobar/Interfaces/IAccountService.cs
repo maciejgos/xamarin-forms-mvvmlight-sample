5d4413b124e43b5d383e9f06ad2b6d117b12a6fb
Maciej Gos <maciej@maciejgos.com>
2017-01-16 21:55:44 +0100

    Create IAccountService interface

---
+++ src/Client_BankOfSanEscobar/Client_BankOfSanEscobar/Interfaces/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Client_BankOfSanEscobar.Models;
+
+namespace Client_BankOfSanEscobar.Interfaces
+{
+    public class IAccountService
+    {
+        public Task<IEnumerable<Account>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
