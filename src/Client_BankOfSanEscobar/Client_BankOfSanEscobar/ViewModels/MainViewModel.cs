b8f3ac14c0d1c4cede005ed06c7ddbdb9abb7a69
Maciej Gos <maciej@maciejgos.com>
2017-01-19 06:10:14 +0100

    Create accounts view

--- src/Client_BankOfSanEscobar/Client_BankOfSanEscobar/ViewModels/MainViewModel.cs
+++ src/Client_BankOfSanEscobar/Client_BankOfSanEscobar/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using Client_BankOfSanEscobar.Models;
 using GalaSoft.MvvmLight;
 using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Client_BankOfSanEscobar.ViewModels
@@ -13,7 +14,8 @@ namespace Client_BankOfSanEscobar.ViewModels
 
         public string Title { get; set; }
         public ObservableCollection<Account> Accounts { get; set; }
-
+        public float AvailableBalance { get; set; }
+        
         public MainViewModel(IAccountService accountService)
         {
             if (accountService == null)
@@ -30,6 +32,8 @@ namespace Client_BankOfSanEscobar.ViewModels
                 return;
 
             Accounts = new ObservableCollection<Account>(await accountService.GetAll());
+            foreach (var balance in Accounts.Select(a => a.Balance))
+                AvailableBalance += balance;
         }
     }
 }
